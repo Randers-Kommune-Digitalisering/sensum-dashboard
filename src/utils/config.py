a3493ac72484756d8b0cfa7b03d71e9ed4059f0d
nk-randers <nivethan.kandiah@randers.dk>
2025-02-25 12:03:51 +0100

    update config

--- src/utils/config.py
+++ src/utils/config.py
@@ -20,3 +20,14 @@ DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 DB_NAME = os.environ.get('DB_NAME')
+
+SENSUM_IT_SFTP_REMOTE_DIR = '/D:/SFTP-EGDW/'
+SENSUM_IT_SFTP_HOST = os.environ['SENSUM_IT_SFTP_HOST'].rstrip()
+SENSUM_IT_SFTP_USER = os.environ['SENSUM_IT_SFTP_USER'].rstrip()
+SENSUM_IT_SFTP_PASS = os.environ['SENSUM_IT_SFTP_PASS'].rstrip()
+SENSUM_CONFIG_FILE = 'sensum_jobs_config.json'
+
+CONFIG_LIBRARY_BASE_PATH = 'api/file/etl/'
+CONFIG_LIBRARY_USER = os.environ['CONFIG_LIBRARY_USER'].rstrip()
+CONFIG_LIBRARY_PASS = os.environ['CONFIG_LIBRARY_PASS'].rstrip()
+CONFIG_LIBRARY_URL = os.environ['CONFIG_LIBRARY_URL'].rstrip()
