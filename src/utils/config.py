3020f11eb7dad08f1c77c97789082d40283ee5a6
nk-randers <nivethan.kandiah@randers.dk>
2025-02-26 13:40:15 +0100

    update DatabaseClient and config

--- src/utils/config.py
+++ src/utils/config.py
@@ -21,11 +21,10 @@ DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
 DB_NAME = os.environ.get('DB_NAME')
 
-SENSUM_IT_SFTP_REMOTE_DIR = '/D:/SFTP-EGDW/'
-SENSUM_IT_SFTP_HOST = os.environ['SENSUM_IT_SFTP_HOST'].rstrip()
-SENSUM_IT_SFTP_USER = os.environ['SENSUM_IT_SFTP_USER'].rstrip()
-SENSUM_IT_SFTP_PASS = os.environ['SENSUM_IT_SFTP_PASS'].rstrip()
-SENSUM_CONFIG_FILE = 'sensum_jobs_config.json'
+SENSUM_DB_USER = os.environ['SENSUM_DB_USER'].rstrip()
+SENSUM_DB_PASS = os.environ['SENSUM_DB_PASS'].rstrip()
+SENSUM_DB_HOST = os.environ['SENSUM_DB_HOST'].rstrip()
+SENSUM_DB_DATABASE = os.environ['SENSUM_DB_DATABASE'].rstrip()
 
 CONFIG_LIBRARY_BASE_PATH = 'api/file/etl/'
 CONFIG_LIBRARY_USER = os.environ['CONFIG_LIBRARY_USER'].rstrip()
