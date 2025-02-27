82f3e870a28585a962525969615ea04a1a98aeb6
nk-randers <nivethan.kandiah@randers.dk>
2025-02-27 09:42:10 +0100

    add styling to menu

--- src/main.py
+++ src/main.py
@@ -8,10 +8,18 @@ st.set_page_config(page_title="Sensum Dashboard", page_icon="assets/favicon.ico"
 with st.sidebar:
     st.sidebar.markdown(get_logo(), unsafe_allow_html=True)
     selected = option_menu(
-        "Sensum data",
+        "Sensum Dashboard",
         ["Antal igangværende sager", "Indsatser", "Ydelse fordelt på afdeling"],
-        icons=['house'],
-        default_index=0
+        default_index=0,
+        icons=['list-task', 'activity', 'bar-chart'],
+        menu_icon="cast",
+        styles={
+            "container": {"padding": "5px", "background-color": "#f0f0f0"},
+            "icon": {"color": "#4a4a4a", "font-size": "18px"},
+            "nav-link": {"font-size": "18px", "text-align": "left", "margin": "0px", "--hover-color": "#e0e0e0"},
+            "nav-link-selected": {"background-color": "#d0d0d0", "color": "#4a4a4a"},
+            "menu-title": {"font-size": "20px", "font-weight": "bold", "color": "#4a4a4a", "margin-bottom": "10px"},
+        }
     )
 
 if selected == "Antal igangværende sager":
