87031f2a9706ea38fa4ed73da52dc533b5c06318
nk-randers <nivethan.kandiah@randers.dk>
2025-02-27 10:07:57 +0100

    remove sftp_connection

--- src/utils/sftp_connection.py
+++
@@ -1,6 +0,0 @@
-from utils.config import SENSUM_IT_SFTP_HOST, SENSUM_IT_SFTP_USER, SENSUM_IT_SFTP_PASS
-from utils.sftp import SFTPClient
-
-
-def get_sftp_client():
-    return SFTPClient(SENSUM_IT_SFTP_HOST, SENSUM_IT_SFTP_USER, password=SENSUM_IT_SFTP_PASS)
